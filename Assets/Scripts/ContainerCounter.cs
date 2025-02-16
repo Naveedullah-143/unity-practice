a65cfc30743223013e6988430e8de20638cd3802
Naveedullah-143 <iamdevilyt456@gmail.com>
2025-02-16 21:14:56 +0500

    new feauters

--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -1,21 +1,26 @@
 using System;
 using UnityEngine;
 
-public class ContainerCounter : BaseCounter{
- 
+public class ContainerCounter : BaseCounter
+{
+
   public event EventHandler OnPlayerGrabbedObject;
-  [SerializeField] private KitchenObjectSO kitchenObjectSO;  
- 
- 
-     public override void Interact(Player player){
-    
-    Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-    kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
-
-    OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
-   }
- }  
+  [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
 
-  
+  public override void Interact(Player player)
+  {
+    if (!player.HasKitchenObject())
+    {
+      //player is not carrying anything
+      Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+      kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+
+      OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+    }
+  }
+}
+
+
+
 

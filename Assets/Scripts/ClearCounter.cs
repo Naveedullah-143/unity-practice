3dcc2e5cb8ed5de6a948c2447d221bae67e09e93
Naveedullah-143 <iamdevilyt456@gmail.com>
2025-02-16 21:00:58 +0500

    add some code

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -6,17 +6,23 @@ public class ClearCounter : BaseCounter
  [SerializeField] private KitchenObjectSO kitchenObjectSO;  
 
 
- public override void Interact(Player player){
+public override void Interact(Player player){
    if(!HasKitchenObject()){
     //there is no kitchen object here
     if(player.HasKitchenObject()){
         //player is carraying something
         player.GetKitchenObject().SetKitchenObjectParent(this);
+    }else{
+      // player not carrying any 
     }
    }else{
     //there is kitchen object
+    if (player.HasKitchenObject()){
+      // player is carrying  something
+    }else{
+      // player is not carrying anything
+      GetKitchenObject().SetKitchenObjectParent(player);
+    }
    }
- }   
-
- 
+ } 
 }
